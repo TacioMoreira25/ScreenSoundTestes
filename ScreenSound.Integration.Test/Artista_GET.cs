1285026e0b2167349e03a6144f94d6fd5103e76b
tacio <moreiravini144@gmail.com>
2025-02-20 21:45:48 -0300

    first commit

---
+++ ScreenSound.Integration.Test/Artista_GET.cs
@@ -0,0 +1,36 @@
+using System.Net.Http.Json;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Integration.Test;
+
+public class Artista_GET: IClassFixture<ScreenSoundWebApplicationFactory>
+{
+    private readonly ScreenSoundWebApplicationFactory app;
+    
+    public Artista_GET(ScreenSoundWebApplicationFactory app)
+    {
+        this.app = app;
+    }
+    [Fact]
+    public async Task Recupera_Artista_Por_Nome()
+    {
+        // Arrange
+        var artistaExistente = app.Context.Artistas.FirstOrDefault();
+        if (artistaExistente is null) {
+        
+            artistaExistente = new Artista() { Nome = "João Silva", Bio = "Nascido em SP." };
+            app.Context.Add(artistaExistente);
+            app.Context.SaveChanges();
+        }
+        
+        using var client = app.CreateClient();
+        
+        //Act
+        var response = await client.GetFromJsonAsync<Artista>("/Artistas/" + artistaExistente.Nome);
+        
+        //Assert
+        Assert.NotNull(response);
+        Assert.Equal(artistaExistente.Id, response.Id);
+        Assert.Equal(artistaExistente.Nome, response.Nome);
+    }
+}
---
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
+
+
+namespace ScreenSound.Banco;
+public class ScreenSoundContext: DbContext
+{
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+
+    private string connectionString = "Server=localhost,1433;Database=ScreenSound_Testes;User Id=SA;Password=DbServer123;Encrypt=False;";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Musica>()
+            .HasMany(c => c.Generos)
+            .WithMany(c => c.Musicas);
+    }
+
+}
